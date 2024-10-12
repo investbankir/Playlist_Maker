0b7b83de3b40d19e7a135d4752752327dfbf9ccc
Investbankir <leopardo4174@ya.ru>
2024-10-12 21:17:05 +0500

    Спринт 22

---
+++ app/src/main/java/com/example/playlistmaker/create_new_playlist/ui/CreateNewPlaylistFragment.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker.create_new_playlist.ui
+
+import androidx.fragment.app.Fragment
+
+class CreateNewPlaylistFragment: Fragment() {
+    //Реализация функций класса - экрана создания нового плейлиста
+}
--- app/src/main/java/com/example/playlistmaker/media_library/ui/PlayListFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media_library/ui/PlayListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.R
+import androidx.navigation.fragment.findNavController
 import com.example.playlistmaker.databinding.FragmentPlaylistBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -20,6 +22,14 @@ class PlayListFragment : Fragment() {
     ): View? {
         _binding = FragmentPlaylistBinding.inflate(inflater, container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.newPlayListButton.setOnClickListener {
+            findNavController().navigate(R.id.action_playListFragment_to_createNewPlaylistFragment)
+        }
     }
 
     override fun onDestroyView() {
